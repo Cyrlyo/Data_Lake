cc45e2e413679508d8b6bd7c639275a082483377
root <root@DESKTOP-M7L2JAQ.localdomain>
2022-11-07 12:53:45 +0100

    Zip downloader

--- importation.py
+++ importation.py
@@ -2,4 +2,28 @@ import pandas as pd
 import numpy as np
 import random
 import lxml
+import requests
 
+def importData(url: str):
+    
+    data = pd.read_html(url)
+    
+    return data
+
+def importhtml(url: str):
+    
+    print("coucou1")
+    response = requests.get(url)
+    print("coucou2")
+    open("allCountries.zip", "wb").write(response.content)
+    print("coucou3")
+
+
+if __name__ == "__main__":
+    
+    print("coucou")
+    # test = importData("https://www.kaggle.com/datasets/shmalex/instagram-dataset?select=instagram_locations.csv")
+    # print(test)
+    
+    importhtml("http://download.geonames.org/export/dump/allCountries.zip")
+    
