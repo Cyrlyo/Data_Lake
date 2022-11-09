483e940fba0001dc52d4f580f0ca35786d08d578
Marin <marinmainka@gmail.com>
2022-11-09 11:22:21 +0100

    Creating folder function

--- importation.py
+++ importation.py
@@ -12,20 +12,31 @@ def importData(url: str):
     
     return data
 
+def createFile(folder_name: str, parent_dir: str):
+    
+    current_dir = os.getcwd()
+    print(f"You're current directory is: {current_dir}")
+    path = os.path.join(os.path.join(current_dir, parent_dir), folder_name)
+    os.mkdir(path)
+    print(f"'{folder_name}' has been created\nPath: {path}")
+    
+
 def importhtml(url: str):
     
     print("coucou1")
     response = requests.get(url)
+    os.mkdir("Raw")
     print("coucou2")
-    open("allCountries.zip", "wb").write(response.content)
+    open("./Data/RAw/allCountries.zip", "wb").write(response.content)
     print("coucou3")
+
 
 def unZip(file_name: list):
     """"
     arguments: 
     - file_name: a list of string of files' name with it extension we're looking for unzip.
     """
-
+    os.chdir("Data/Raw")
     files_name = os.listdir()
     extension = [".zip", ".tar.gz"]
     
@@ -43,6 +54,7 @@ if __name__ == "__main__":
     # test = importData("https://www.kaggle.com/datasets/shmalex/instagram-dataset?select=instagram_locations.csv")
     # print(test)
     
+    createFile("Raw", "Data")
     # importhtml("http://download.geonames.org/export/dump/allCountries.zip")
-    unZip(["allCountries.zip"])
+    # unZip(["allCountries.zip"])
     
