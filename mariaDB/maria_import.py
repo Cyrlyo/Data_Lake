4a638cc8d1fdeeb5a896ad9a0975685e92157cb3
Marin <marinmainka@gmail.com>
2022-11-13 19:57:10 +0100

    import yaml update gitignore & requirements

--- mariaDB/maria_import.py
+++ mariaDB/maria_import.py
@@ -1,2 +1,27 @@
 import mariadb
+import yaml
 
+
+print("coucou0")
+connexion = mariadb.connect(
+    user = "root",
+    password = "root",
+    host = "localhost",
+    port = 3306
+)
+print("coucou1")
+print(connexion)
+
+print("coucou3")
+cursor = connexion.cursor()
+print("coucour4")
+print(cursor)
+
+print("coucou5")
+cursor.execute("SHOW DATABASES")
+print("coucou6")
+
+data_base_list = cursor.fetchall()
+  
+for database in data_base_list:
+  print(database)
