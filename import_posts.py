e2cc235142923b6426923b5914d40a2e50892f95
Marin <marinmainka@gmail.com>
2022-11-15 18:53:36 +0100

    add functionnal posts import files NEED TO MOVE IT IN import_data but module problems

---
+++ import_posts.py
@@ -0,0 +1,13 @@
+import os
+from import_data.importation import mainDL
+from import_data.utils import unZip, deletingFiles, deplaceFiles
+
+def importPosts(dataset_name: str, source: str, files_name: list):
+    os.chdir("./Data/Raw")
+    mainDL(dataset_name, files_name)
+    unZip([source])
+
+    deletingFiles(source)
+    os.chdir("../../")
+
+importPosts("http://d3smaster.fr", "posts_reduced.zip", ["posts_reduced.zip"])
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from import_data.importation import poi_import
 from import_data.api_import import api_import
 from kaggle.api import KaggleApi
 from mariaDB.maria_import import importToMariaDB
+from import_posts import importPosts
 
 DATASET_NAME = "shmalex/instagram-dataset"
 SOURCE_1 = "instagram_locations.csv"
@@ -10,6 +11,8 @@ SOURCES = [SOURCE_1, SOURCE_2]
 DATASET_NAME_2 = "http://download.geonames.org/export/dump"
 SOURCE_3 = "allCountries.zip"
 FILES_NAME = ["allCountries.zip", "readme.txt"]
+DATASET_NAME_3 = "http://d3smaster.fr"
+SOURCE_4 = "posts_reduced.zip"
 
 # TODO: voir TODO importation.py
 
@@ -21,5 +24,6 @@ except:
 
 poi_import(DATASET_NAME_2, SOURCE_3, FILES_NAME)
 api_import(API, DATASET_NAME, SOURCES)
+importPosts(DATASET_NAME_3, SOURCE_4, [SOURCE_4])
 
 importToMariaDB("point_of_interest", "allCountries", "./Data/Raw/allCountries/allCountries.txt")
