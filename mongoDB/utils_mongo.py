7715626ac3d3ad6e6ce10068e7d888c5a27ad613
Marin <marinmainka@gmail.com>
2022-11-18 11:55:52 +0100

    csvtojson working function with path creator

--- mongoDB/utils_mongo.py
+++ mongoDB/utils_mongo.py
@@ -1,11 +1,18 @@
 import csv
 import json
+import os
+from pathlib import Path
 
 def csvToJson(csv_path_file: str, json_path_file: str):
+    """_summary_
+
+    Args:
+        csv_path_file (str): _description_
+        json_path_file (str): _description_
+    """
     
     json_array = []
     
-    # print(f"Opening {csv_path_file}")
     with open(csv_path_file, encoding="utf-8") as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter="\t")
         
@@ -13,7 +20,29 @@ def csvToJson(csv_path_file: str, json_path_file: str):
             json_array.append(row)
     print("\nExtracting json... please wait\nIt can take time")
     
+    result, createPath = pathChecker(json_path_file)
+    if not result:
+        os.makedirs(createPath)
+    
     with open(json_path_file, "w", encoding="utf-8") as json_file:
         json_string = json.dumps(json_array, ensure_ascii=False)
         json_file.write(json_string)
-        
+
+def pathChecker(path: str) -> bool:
+    """_summary_
+
+    Args:
+        path (str): _description_
+
+    Returns:
+        bool: _description_
+    """
+    
+    result_dict = {True: "exists", False: "doesn't exist"}
+    
+    splited_path = Path(path).parts
+    formated_path = os.path.join(*splited_path[: -1])
+    result = os.path.exists(formated_path)
+    print(f"\n{formated_path} {result_dict[result]}")
+    
+    return result, formated_path
